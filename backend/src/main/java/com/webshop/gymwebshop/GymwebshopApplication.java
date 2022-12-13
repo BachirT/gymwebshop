9f95962784f4145b898ac949677599e0cab6a4b2
BachirT <31505903+BachirT@users.noreply.github.com>
2022-12-13 18:22:06 +0100

    Add files via upload
    Starting a new project for a webshop

---
+++ backend/src/main/java/com/webshop/gymwebshop/GymwebshopApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.gymwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymwebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymwebshopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/webshop/gymwebshop/GymwebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.gymwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymwebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
