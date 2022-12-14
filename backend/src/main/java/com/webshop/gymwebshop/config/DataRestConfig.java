a17897ee9e2b340bab65f6c68d5b787dbf786686
DonDLokko <120112954+DonDLokko@users.noreply.github.com>
2022-12-14 13:19:41 +0100

    added config for put/del/post

--- backend/src/main/java/com/webshop/gymwebshop/config/DataRestConfig.java
+++ backend/src/main/java/com/webshop/gymwebshop/config/DataRestConfig.java
@@ -1,2 +1,35 @@
-package com.webshop.gymwebshop.config;public class DataRestConfig {
+package com.webshop.gymwebshop.config;
+
+import com.webshop.gymwebshop.entity.Product;
+import com.webshop.gymwebshop.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        // Dit zijn de httpMethods die niet zomaar gedaan mogen worden:
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+
+        // De httpMethods mogen niet gebruikt worden per item of hele lijst voor product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // De httpMethods mogen niet gebruikt worden per item of hele lijst voor product-category
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+
+    }
 }
